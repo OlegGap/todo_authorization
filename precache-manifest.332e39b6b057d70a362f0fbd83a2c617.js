4893f2028721a611a759efaac02f2e6db5661e63
oleg <hnapovskiy20@gmail.com>
2019-10-28 19:42:22 +0200

    Updates

---
+++ precache-manifest.332e39b6b057d70a362f0fbd83a2c617.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85357383347473d956228f15aba3ff1c",
+    "url": "/todo_authorization/index.html"
+  },
+  {
+    "revision": "7df8189df7269ad2b4d1",
+    "url": "/todo_authorization/static/css/2.ced99b4c.chunk.css"
+  },
+  {
+    "revision": "5ac098ae80f79611fd28",
+    "url": "/todo_authorization/static/css/main.f2ae8de3.chunk.css"
+  },
+  {
+    "revision": "7df8189df7269ad2b4d1",
+    "url": "/todo_authorization/static/js/2.74fdca49.chunk.js"
+  },
+  {
+    "revision": "5ac098ae80f79611fd28",
+    "url": "/todo_authorization/static/js/main.c55d27b4.chunk.js"
+  },
+  {
+    "revision": "f5e095262a374707ca3d",
+    "url": "/todo_authorization/static/js/runtime-main.f4ee71e2.js"
+  }
+]);
